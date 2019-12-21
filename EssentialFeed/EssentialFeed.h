2d610145ffa02f11980c886454db1bd2407596db
Raphael Silva <sr.silvaraphael@gmail.com>
2019-12-21 15:43:26 +0100

    Initial Commit

---
+++ EssentialFeed/EssentialFeed.h
@@ -0,0 +1,19 @@
+//
+//  EssentialFeed.h
+//  EssentialFeed
+//
+//  Created by Raphael Silva on 21/12/2019.
+//  Copyright © 2019 Raphael Silva. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeed.
+FOUNDATION_EXPORT double EssentialFeedVersionNumber;
+
+//! Project version string for EssentialFeed.
+FOUNDATION_EXPORT const unsigned char EssentialFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeed/PublicHeader.h>
+
+
