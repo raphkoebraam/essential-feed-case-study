b28c5d99912af6e1f8af941b096f5127f97b1941
Raphael Silva <sr.silvaraphael@gmail.com>
2019-12-22 12:18:57 +0100

    Move RemoteFeedLoader components to production

---
+++ EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -0,0 +1,28 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Raphael Silva on 22/12/2019.
+//  Copyright © 2019 Raphael Silva. All rights reserved.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public final class RemoteFeedLoader {
+    
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
