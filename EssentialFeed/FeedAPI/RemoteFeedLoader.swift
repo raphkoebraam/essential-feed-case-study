06d00057d57408fd79a964278a783e6df378808f
Raphael Silva <sr.silvaraphael@gmail.com>
2019-12-22 17:24:55 +0100

    Moves mapping logic to a new type FeedItemsMapper

--- EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -41,9 +41,10 @@ public final class RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success(data, response):
-                if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
-                    completion(.succes(root.items.map { $0.feedItem }))
-                } else {
+                do {
+                    let items = try FeedItemsMapper.map(data: data, response: response)
+                    completion(.succes(items))
+                } catch {
                     completion(.failure(.invalidData))
                 }
             case .failure:
@@ -53,17 +54,29 @@ public final class RemoteFeedLoader {
     }
 }
 
-private struct Root: Decodable {
-    let items: [Item]
-}
-
-private struct Item: Decodable {
-    public let id: UUID
-    public let description: String?
-    public let location: String?
-    public let image: URL
+private class FeedItemsMapper {
     
-    var feedItem: FeedItem {
-        return FeedItem(id: id, description: description, location: location, imageURL: image)
+    private struct Root: Decodable {
+        let items: [Item]
+    }
+
+    private struct Item: Decodable {
+        public let id: UUID
+        public let description: String?
+        public let location: String?
+        public let image: URL
+        
+        var feedItem: FeedItem {
+            return FeedItem(id: id, description: description, location: location, imageURL: image)
+        }
+    }
+    
+    static func map(data: Data, response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == 200 else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+        
+        let root = try JSONDecoder().decode(Root.self, from: data)
+        return root.items.map { $0.feedItem }
     }
 }
