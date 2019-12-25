0adde84244bb8c8730c4ee3de7076d9e7ed0bdb5
Raphael Silva <sr.silvaraphael@gmail.com>
2019-12-25 16:58:36 +0100

    Moves RemoteFeedItem to its own file

---
+++ EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  RemoteFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Raphael Silva on 25/12/2019.
+//  Copyright © 2019 Raphael Silva. All rights reserved.
+//
+
+import Foundation
+
+internal struct RemoteFeedItem: Decodable {
+    internal let id: UUID
+    internal let description: String?
+    internal let location: String?
+    internal let image: URL
+}
