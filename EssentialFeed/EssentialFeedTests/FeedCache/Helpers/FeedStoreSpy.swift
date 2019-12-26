6dd0f45b8402588d4ec7e8a33f63e90dbbd50401
Raphael Silva <sr.silvaraphael@gmail.com>
2019-12-26 12:46:55 +0100

    Load command requests cache retrieval

--- EssentialFeed/EssentialFeedTests/FeedCache/Helpers/FeedStoreSpy.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/Helpers/FeedStoreSpy.swift
@@ -13,6 +13,7 @@ class FeedStoreSpy: FeedStore {
     enum ReceivedMessage: Equatable {
         case deleteCachedFeed
         case insert([LocalFeedImage], Date)
+        case retrieve
     }
     
     private(set) var receivedMessages = [ReceivedMessage]()
@@ -44,5 +45,9 @@ class FeedStoreSpy: FeedStore {
     
     func completeInsertionSuccessfully(at index: Int = 0) {
         insertionCompletions[index](nil)
+    }
+    
+    func retrieve() {
+        receivedMessages.append(.retrieve)
     }
 }
