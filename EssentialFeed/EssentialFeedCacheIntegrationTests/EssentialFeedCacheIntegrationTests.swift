3dcffcd56e0b2b3e9d3189a5c26f0607e86eefa3
Raphael Silva <sr.silvaraphael@gmail.com>
2020-03-22 13:56:46 +0100

    Adds `EssentialFeedCachedIntegrationTests` target to separate the potentially slower cache integration tests from the fast unit/isolated tests.

---
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Raphael Silva on 22/03/2020.
+//  Copyright © 2020 Raphael Silva. All rights reserved.
+//
+
+import XCTest
+
+class EssentialFeedCacheIntegrationTests: XCTestCase {
+
+}
