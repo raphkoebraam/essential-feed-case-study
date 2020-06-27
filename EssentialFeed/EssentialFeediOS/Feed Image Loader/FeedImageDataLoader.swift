f0ad69c9015b82bdf597dab846fd2e53f9cc15e0
Raphael Silva <sr.silvaraphael@gmail.com>
2020-06-27 16:52:42 +0200

    Update project structure.

---
+++ EssentialFeed/EssentialFeediOS/Feed Image Loader/FeedImageDataLoader.swift
@@ -0,0 +1,19 @@
+//
+//  FeedImageDataLoader.swift
+//  EssentialFeediOS
+//
+//  Created by Raphael Silva on 27/06/2020.
+//  Copyright © 2020 Raphael Silva. All rights reserved.
+//
+
+import Foundation
+
+public protocol FeedImageDataLoaderTask {
+    func cancel()
+}
+
+public protocol FeedImageDataLoader {
+    typealias Result = Swift.Result<Data, Error>
+    
+    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+}
