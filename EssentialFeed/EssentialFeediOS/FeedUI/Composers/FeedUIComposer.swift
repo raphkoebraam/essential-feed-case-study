7630408afcdfc25a9b4b83c44a0379bb323f60f6
Raphael Silva <sr.silvaraphael@gmail.com>
2020-06-28 22:31:36 +0200

    Dispatch background feed image data completion to main thread before passing result to the UI components. Threading is dealt with a Decorator in the Composition layer.

--- EssentialFeed/EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
@@ -21,7 +21,7 @@ public final class FeedUIComposer {
         
         feedController.delegate = presentationAdapter
         presentationAdapter.presenter = FeedPresenter(feedView: FeedViewAdapter(controller: feedController,
-                                                                                imageLoader: imageLoader),
+                                                                                imageLoader: MainQueueDispatchDecorator(decoratee: imageLoader)),
                                                       loadingView: WeakReferenceVirtualProxy(feedController))
         
         return feedController
@@ -47,6 +47,16 @@ private final class MainQueueDispatchDecorator<T> {
 extension MainQueueDispatchDecorator: FeedLoader where T == FeedLoader {
     func load(completion: @escaping (FeedLoader.Result) -> Void) {
         decoratee.load { [weak self] (result) in
+            self?.dispatch {
+                completion(result)
+            }
+        }
+    }
+}
+
+extension MainQueueDispatchDecorator: FeedImageDataLoader where T == FeedImageDataLoader {
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        return decoratee.loadImageData(from: url) { [weak self] (result) in
             self?.dispatch {
                 completion(result)
             }
