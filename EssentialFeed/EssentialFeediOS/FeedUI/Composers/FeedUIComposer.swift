ae137c1f9904803bc793ce68fe9cf5de517768e1
Raphael Silva <sr.silvaraphael@gmail.com>
2020-06-28 22:41:34 +0200

    Move `WeakReferenceVirtualProxy` to separate file.

--- EssentialFeed/EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
@@ -38,26 +38,6 @@ private extension FeedTableViewController {
     }
 }
 
-private final class WeakReferenceVirtualProxy<T: AnyObject> {
-    private weak var object: T?
-    
-    init(_ object: T) {
-        self.object = object
-    }
-}
-
-extension WeakReferenceVirtualProxy: FeedLoadingView where T: FeedLoadingView {
-    func display(_ viewModel: FeedLoadingViewModel) {
-        object?.display(viewModel)
-    }
-}
-
-extension WeakReferenceVirtualProxy: FeedImageView where T: FeedImageView, T.Image == UIImage {
-    func display(_ viewModel: FeedImageViewModel<UIImage>) {
-        object?.display(viewModel)
-    }
-}
-
 private final class FeedViewAdapter: FeedView {
     
     private weak var controller: FeedTableViewController?
---
+++ EssentialFeed/EssentialFeediOS/FeedUI/Composers/WeakReferenceVirtualProxy.swift
@@ -0,0 +1,29 @@
+//
+//  WeakReferenceVirtualProxy.swift
+//  EssentialFeediOS
+//
+//  Created by Raphael Silva on 28/06/2020.
+//  Copyright © 2020 Raphael Silva. All rights reserved.
+//
+
+import UIKit
+
+final class WeakReferenceVirtualProxy<T: AnyObject> {
+    private weak var object: T?
+    
+    init(_ object: T) {
+        self.object = object
+    }
+}
+
+extension WeakReferenceVirtualProxy: FeedLoadingView where T: FeedLoadingView {
+    func display(_ viewModel: FeedLoadingViewModel) {
+        object?.display(viewModel)
+    }
+}
+
+extension WeakReferenceVirtualProxy: FeedImageView where T: FeedImageView, T.Image == UIImage {
+    func display(_ viewModel: FeedImageViewModel<UIImage>) {
+        object?.display(viewModel)
+    }
+}
