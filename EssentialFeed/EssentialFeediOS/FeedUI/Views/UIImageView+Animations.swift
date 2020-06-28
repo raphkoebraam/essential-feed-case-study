66e1e52016e6f7ff17f7c5e99dc6cd2cd1fef2b5
Raphael Silva <sr.silvaraphael@gmail.com>
2020-06-28 15:38:21 +0200

    Display image with animation.

---
+++ EssentialFeed/EssentialFeediOS/FeedUI/Views/UIImageView+Animations.swift
@@ -0,0 +1,22 @@
+//
+//  UIImageView+Animations.swift
+//  EssentialFeediOS
+//
+//  Created by Raphael Silva on 28/06/2020.
+//  Copyright © 2020 Raphael Silva. All rights reserved.
+//
+
+import UIKit
+
+extension UIImageView {
+    func setImageAnimated(_ newImage: UIImage?) {
+        image = newImage
+        
+        guard newImage != nil else { return }
+        
+        alpha = 0
+        UIView.animate(withDuration: 0.25) {
+            self.alpha = 1
+        }
+    }
+}
