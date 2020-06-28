91b3f453cc99927d862730a674a7656bce474ce9
Raphael Silva <sr.silvaraphael@gmail.com>
2020-06-28 22:33:36 +0200

    Move `MainQueueDispatchDecorator` to separate file.

--- EssentialFeed/EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
@@ -28,42 +28,6 @@ public final class FeedUIComposer {
     }
 }
 
-private final class MainQueueDispatchDecorator<T> {
-    private let decoratee: T
-    
-    init(decoratee: T) {
-        self.decoratee = decoratee
-    }
-    
-    func dispatch(completion: @escaping () -> Void) {
-        guard Thread.isMainThread else {
-            return DispatchQueue.main.async(execute: completion)
-        }
-        
-        completion()
-    }
-}
-
-extension MainQueueDispatchDecorator: FeedLoader where T == FeedLoader {
-    func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        decoratee.load { [weak self] (result) in
-            self?.dispatch {
-                completion(result)
-            }
-        }
-    }
-}
-
-extension MainQueueDispatchDecorator: FeedImageDataLoader where T == FeedImageDataLoader {
-    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        return decoratee.loadImageData(from: url) { [weak self] (result) in
-            self?.dispatch {
-                completion(result)
-            }
-        }
-    }
-}
-
 private extension FeedTableViewController {
     static func makeWith(delegate: FeedTableViewControllerDelegate, title: String) -> FeedTableViewController {
         let bundle = Bundle(for: FeedTableViewController.self)
