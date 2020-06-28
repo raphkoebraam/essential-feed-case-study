5fcc78178482ff654cbb6cfea64a30a30b573f18
Raphael Silva <sr.silvaraphael@gmail.com>
2020-06-28 22:46:38 +0200

    Move `FeedViewAdapter` to separate file.

--- EssentialFeed/EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
+++ EssentialFeed/EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
@@ -37,26 +37,3 @@ private extension FeedTableViewController {
         return feedController
     }
 }
-
-private final class FeedViewAdapter: FeedView {
-    
-    private weak var controller: FeedTableViewController?
-    private let imageLoader: FeedImageDataLoader
-    
-    init(controller: FeedTableViewController, imageLoader: FeedImageDataLoader) {
-        self.controller = controller
-        self.imageLoader = imageLoader
-    }
-    
-    func display(_ viewModel: FeedViewModel) {
-        controller?.tableModel = viewModel.feed.map { model in
-            let adapter = FeedImageDataLoaderPresentationAdapter<WeakReferenceVirtualProxy<FeedImageCellController>, UIImage>(model: model, imageLoader: imageLoader)
-            let view = FeedImageCellController(delegate: adapter)
-            
-            adapter.presenter = FeedImagePresenter(view: WeakReferenceVirtualProxy(view),
-                                                   imageTransformer: UIImage.init)
-            
-            return view
-        }
-    }
-}
---
+++ EssentialFeed/EssentialFeediOS/FeedUI/Composers/FeedViewAdapter.swift
@@ -0,0 +1,32 @@
+//
+//  FeedViewAdapter.swift
+//  EssentialFeediOS
+//
+//  Created by Raphael Silva on 28/06/2020.
+//  Copyright © 2020 Raphael Silva. All rights reserved.
+//
+
+import UIKit
+
+final class FeedViewAdapter: FeedView {
+    
+    private weak var controller: FeedTableViewController?
+    private let imageLoader: FeedImageDataLoader
+    
+    init(controller: FeedTableViewController, imageLoader: FeedImageDataLoader) {
+        self.controller = controller
+        self.imageLoader = imageLoader
+    }
+    
+    func display(_ viewModel: FeedViewModel) {
+        controller?.tableModel = viewModel.feed.map { model in
+            let adapter = FeedImageDataLoaderPresentationAdapter<WeakReferenceVirtualProxy<FeedImageCellController>, UIImage>(model: model, imageLoader: imageLoader)
+            let view = FeedImageCellController(delegate: adapter)
+            
+            adapter.presenter = FeedImagePresenter(view: WeakReferenceVirtualProxy(view),
+                                                   imageTransformer: UIImage.init)
+            
+            return view
+        }
+    }
+}
