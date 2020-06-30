6145e908d62f4385b964bffb036f24e5b698516c
Raphael Silva <sr.silvaraphael@gmail.com>
2020-06-30 21:37:54 +0200

    Move `FeedErrorViewModel`  to separate file.

---
+++ EssentialFeed/EssentialFeediOS/FeedPresentation/FeedErrorViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Raphael Silva on 30/06/2020.
+//  Copyright © 2020 Raphael Silva. All rights reserved.
+//
+
+struct FeedErrorViewModel {
+    let message: String?
+}
