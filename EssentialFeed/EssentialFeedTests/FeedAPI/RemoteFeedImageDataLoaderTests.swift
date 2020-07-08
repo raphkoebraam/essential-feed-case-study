9a4cb4281d4dfffb262ccf6f57f8fbe7f627699b
Raphael Silva <sr.silvaraphael@gmail.com>
2020-07-08 22:11:59 +0200

    Extract duplicate `anyData` helper into a shared scope.

--- EssentialFeed/EssentialFeedTests/FeedAPI/RemoteFeedImageDataLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedAPI/RemoteFeedImageDataLoaderTests.swift
@@ -152,10 +152,6 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
         return (sut, client)
     }
     
-    private func anyData() -> Data {
-        return Data("any data".utf8)
-    }
-    
     private func failure(_ error: RemoteFeedImageDataLoader.Error) -> FeedImageDataLoader.Result {
         return .failure(error)
     }
