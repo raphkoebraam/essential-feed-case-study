6b5897254ce0f7deddc733f99e82e941d60afe89
Raphael Silva <sr.silvaraphael@gmail.com>
2020-07-09 17:15:52 +0200

    Make `RemoteFeedImageDataLoader` conform to `FeedImageDataLoader`

--- EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedImageDataLoader.swift
+++ EssentialFeed/EssentialFeed/FeedAPI/RemoteFeedImageDataLoader.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-public final class RemoteFeedImageDataLoader {
+public final class RemoteFeedImageDataLoader: FeedImageDataLoader {
     
     public enum Error: Swift.Error {
         case invalidData
