cc8c7fa6cc87345f041be2e62dc772762f987e70
Raphael Silva <sr.silvaraphael@gmail.com>
2020-07-12 11:27:54 +0200

    Add empty EssentialApp project

---
+++ EssentialApp/EssentialApp/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  EssentialApp
+//
+//  Created by Raphael Silva on 12/07/2020.
+//  Copyright © 2020 Raphael Silva. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {}
