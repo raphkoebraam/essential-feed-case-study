293b3402fda6398ac3cc3834557636445e5f4fe3
Raphael Silva <5387088+peagasilva@users.noreply.github.com>
2021-02-21 22:02:46 +0100

    Replace `RemoteLoader` composition with `HTTPClient` publisher composed with `FeedItemsMapper`

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -69,13 +69,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json"
         )!
 
-        let remoteFeedLoader = RemoteFeedLoader(
-            url: remoteURL,
-            client: httpClient
-        )
-
-        return remoteFeedLoader
-            .loadPublisher()
+        return httpClient
+            .getPublisher(url: remoteURL)
+            .tryMap(FeedItemsMapper.map)
             .caching(to: localFeedLoader)
             .fallback(to: localFeedLoader.loadPublisher)
     }
