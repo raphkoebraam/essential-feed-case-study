cbc150664463939377c284c1d262a438a7a02b25
Raphael Silva <5387088+peagasilva@users.noreply.github.com>
2021-02-21 15:57:32 +0100

    Deliver proper result on 2xx responses

--- EssentialFeed/EssentialFeed/FeedAPI/ImageCommentsMapper.swift
+++ EssentialFeed/EssentialFeed/FeedAPI/ImageCommentsMapper.swift
@@ -14,12 +14,22 @@ internal final class ImageCommentsMapper {
         let items: [RemoteFeedItem]
     }
 
-    internal static func map(data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedItem] {
-        guard response.isOK,
-            let root = try? JSONDecoder().decode(Root.self, from: data) else {
-                throw RemoteImageCommentsLoader.Error.invalidData
+    internal static func map(
+        data: Data,
+        from response: HTTPURLResponse
+    ) throws -> [RemoteFeedItem] {
+        guard isOK(response),
+              let root = try? JSONDecoder().decode(Root.self, from: data)
+        else {
+            throw RemoteImageCommentsLoader.Error.invalidData
         }
 
         return root.items
+    }
+
+    private static func isOK(
+        _ response: HTTPURLResponse
+    ) -> Bool {
+        (200...299).contains(response.statusCode)
     }
 }
