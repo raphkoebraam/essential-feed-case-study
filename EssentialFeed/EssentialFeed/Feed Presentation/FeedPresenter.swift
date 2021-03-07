5abdbcafcb87ea10e8ba71ca1d0f2bdc54219b6b
Raphael Silva <5387088+peagasilva@users.noreply.github.com>
2021-03-07 14:15:18 +0100

    Add `FeedPresenter` map

--- EssentialFeed/EssentialFeed/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedPresenter.swift
@@ -28,6 +28,10 @@ public final class FeedPresenter {
                                  bundle: Bundle(for: FeedPresenter.self),
                                  comment: "Message for the error view")
     }
+
+    public static func map(_ feed: [FeedImage]) -> FeedViewModel {
+        FeedViewModel(feed: feed)
+    }
     
     public init(feedView: FeedView, loadingView: ResourceLoadingView, errorView: ResourceErrorView) {
         self.feedView = feedView
@@ -41,7 +45,7 @@ public final class FeedPresenter {
     }
     
     public func didFinishLoadingFeed(with feed: [FeedImage]) {
-        feedView.display(FeedViewModel(feed: feed))
+        feedView.display(Self.map(feed))
         loadingView.display(ResourceLoadingViewModel(isLoading: false))
     }
     
